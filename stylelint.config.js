f2e5e804ef36468405e442ef2bb05746ec8791f4
nikolausliu <18010855044@163.com>
2021-04-18 20:58:27 +0800

    build: add stylelint ignore files

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  ignoreFiles: ['assets/styles/github-markdown.css'],
+  ignoreFiles: ['assets/styles/github-markdown.css', 'assets/fonts/iconfont.css'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {},
