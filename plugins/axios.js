d97bea5ac901880a4b8015d24563c7841ee4708c
nikolausliu <18010855044@163.com>
2021-04-23 10:54:58 +0800

    feat: axios添加客户端错误处理机制

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,10 +1,19 @@
-export default function ({ $axios, error: nuxtError }) {
+export default function ({ $axios, app, error: nuxtError }) {
   $axios.onRequest((config) => {
     console.log('Making request to ' + config.url)
   })
 
   $axios.onError((error) => {
-    console.log('error', error)
+    console.log('error', error.response)
+    if (process.client) {
+      // 处理客户端请求错误
+      console.log(app)
+      if (error.response) {
+        window.$nuxt.$root.$message.error(
+          error.response.data?.error_msg || error.response.statusText
+        )
+      }
+    }
     // nuxtError({
     //   statusCode: error.response.status,
     //   message: error.message,
